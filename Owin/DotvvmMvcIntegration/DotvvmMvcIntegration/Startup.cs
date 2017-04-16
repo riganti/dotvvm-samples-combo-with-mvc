f84fdf59b4935941f61b7b3ba0495cb75069dd86
Tomas Herceg <tomas.herceg@riganti.cz>
2017-04-16 18:15:35 +0200

    Owin sample created

---
+++ Owin/DotvvmMvcIntegration/DotvvmMvcIntegration/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DotvvmMvcIntegration.Startup))]
+
+namespace DotvvmMvcIntegration
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var applicationPath = HostingEnvironment.ApplicationPhysicalPath;
+
+            app.UseDotVVM<DotvvmStartup>(applicationPath);
+        }
+    }
+}

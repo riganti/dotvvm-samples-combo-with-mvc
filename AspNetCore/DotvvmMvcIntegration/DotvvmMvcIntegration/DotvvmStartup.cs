94b386868ca19d942cd5f2e6fc0f4412c2df0af1
Tomáš Herceg <tomas.herceg@riganti.cz>
2020-09-04 14:38:21 +0200

    Update to DotVVM 2.4.0.1 and .NET Core 3.1

--- AspNetCore/DotvvmMvcIntegration/DotvvmMvcIntegration/DotvvmStartup.cs
+++ AspNetCore/DotvvmMvcIntegration/DotvvmMvcIntegration/DotvvmStartup.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using DotVVM.Framework.Configuration;
 using DotVVM.Framework.Routing;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DotvvmMvcIntegration
 {
-    public class DotvvmStartup : IDotvvmStartup
+    public class DotvvmStartup : IDotvvmStartup, IDotvvmServiceConfigurator
     {
         public void Configure(DotvvmConfiguration config, string applicationPath)
         {
@@ -20,6 +21,7 @@ namespace DotvvmMvcIntegration
         {
             config.RouteTable.Add("DotvvmSample", "DotvvmSample", "DotVVM/Views/DotvvmSample.dothtml");
 
+            // URLs that are not registered in DotVVM are passed to the next middleware (MVC)
         }
 
         private void ConfigureControls(DotvvmConfiguration config, string applicationPath)
@@ -30,6 +32,11 @@ namespace DotvvmMvcIntegration
         private void ConfigureResources(DotvvmConfiguration config, string applicationPath)
         {
             // register custom resources and adjust paths to the built-in resources
+        }
+
+        public void ConfigureServices(IDotvvmServiceCollection options)
+        {
+            options.AddDefaultTempStorages("Temp");
         }
     }
 }
--- AspNetCore/DotvvmMvcIntegration/DotvvmMvcIntegration/Startup.cs
+++ AspNetCore/DotvvmMvcIntegration/DotvvmMvcIntegration/Startup.cs
@@ -6,56 +6,47 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace DotvvmMvcIntegration
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
-
-            services.AddDotVVM(options =>
-            {
-                options.AddDefaultTempStorages("Temp");
-            });
+            services.AddDotVVM<DotvvmStartup>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseDotVVM<DotvvmStartup>(env.ContentRootPath);
-            
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+            app.UseRouting();
+            app.UseCors();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseDotVVM<DotvvmStartup>(env.ContentRootPath);
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
